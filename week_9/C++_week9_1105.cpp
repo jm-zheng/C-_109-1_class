c01738fecb9c64620fb011cc4a2486a4261a1569
master_EE603-2_computer <jm.zheng0812@gmail.com>
2021-03-23 22:09:31 +0800

    test

--- week_9/C++_week9_1105.cpp
+++ week_9/C++_week9_1105.cpp
@@ -8,7 +8,7 @@ int Fs(int x) ;
 void com(int *src, int n, int value);
 int bubble_sort_2(int arr[], int size);
 
-void bubble_sort( int arr[]  ,int size)
+/*void bubble_sort( int arr[]  ,int size)
 {
 	for(int i=0;i<size-1 ;i++)
 	{
@@ -29,8 +29,27 @@ void bubble_sort( int arr[]  ,int size)
 			cout << arr[x] << "  ";
 		}
 	}
-}
+}*/
 
+void bubble_sort( int arr[]  ,int size)
+{
+	for(int i=0;i<size-1 ;i++)
+	{
+		for(int j=0;j<size-i-1 ;j++)
+		{
+			if(arr[j] > arr [j+1])
+			{			
+				swap(arr[j] ,arr[j+1]);
+			}
+			
+		}
+		cout <<endl<< "i:"<<i <<" arr: ";
+		for(int x=0;x<10 ;x++)
+		{
+			cout << arr[x] << "  ";
+		}
+	}
+}
 int recursion_sum(int val)
 {
 	if (val == 1)
